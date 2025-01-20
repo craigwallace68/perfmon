ddd8fe2bf6d7355ca5d90bf5424cfe9f000d3056
Craig <46973909+craigwallace68@users.noreply.github.com>
2025-01-19 21:49:52 -0600

    Update db-insert.py
    Ongoing code updates...

--- db-insert.py
+++ db-insert.py
@@ -6,13 +6,14 @@ file_path = '/var/log/syslog'
 field_delimeter = ','
 field_index = 1
 search_ts = sys.argv[1]  # Temporary test, pass in last recorded timestamp in database
+prefix = 'timestamp'
 
 # Binary search file for target timestamp
-def record_search(path, delim, field, ts):
+def record_search(path, delim, field, ts_str):
 
     # Initialize timestamp datatype and list for indexing
     try:
-        ts = datetime.strptime(time_str, '%Y-%m-%d %H:%M:%S')
+        ts_dt = datetime.strptime(ts_str, '%Y-%m-%d %H:%M:%S')
     except ValueError as e01:
         print(f"e01: Invalid date format: {e01}")
     rows_timestamps = []
@@ -21,22 +22,32 @@ def record_search(path, delim, field, ts):
     with open(path, 'r') as f:
         line_number = 1
 
-        # Iterate file, set row line numbers and timestamp paired together as dictionary objects stored in list
+        # Iterate file, set row line numbers and timestamp paired together as dictionary objects_dt stored in list
         while True:
             line = next(f, None)
             if not line:
                 break
-            current_line = line.split(delim)
-            time_str = current_line[field]
-            # Check for relevent row with timestamp prefix to process, else skip row and continue
-            if time_str.startswith('timestamp='):
-                time_str = time_str[10:]
+
+            # Check if line includes the delimeter
+            if prefix in line and delim in line:
+                current_line = line.split(delim)
                 try:
-                    time_record = datetime.strptime(time_str, '%Y-%m-%d %H:%M:%S')
-                except ValueError as e02:
-                    print(f"e02: Invalid date format: {e02}")
-                rows_timestamps.append({'line_number': line_number, 'timestamp': time_record})
-                line_number += 1
+                    time_str = current_line[field]
+                except IndexError as e02:
+                    print(f"e02: Line {line_number}: Field '{field}' not found. {e02}")
+                    continue
+
+                # Check for relevent row with timestamp prefix to process, else skip row and continue
+                if time_str.startswith('timestamp='):
+                    time_str = time_str[10:]
+                    try:
+                        time_record = datetime.strptime(time_str, '%Y-%m-%d %H:%M:%S')
+                    except ValueError as e03:
+                        print(f"e03: Invalid date format: {e03}")
+                        continue
+                    # Add relevent row to list of dictionary objects
+                    rows_timestamps.append({'line_number': line_number, 'timestamp': time_record})
+                    line_number += 1
             else:
                 line_number += 1
 
@@ -45,23 +56,31 @@ def record_search(path, delim, field, ts):
     high_record = len(rows_timestamps) - 1
 
     # Initialize and set range of seconds to include for match (cover 1 minute resolution)
-    ts_start = datetime.strptime(ts, '%Y-%m-%d %H:%M:%S')
-    ts_end = ts_start
-    ts_start = ts.replace(seconds=0)  # start of search minute
-    ts_end = ts_end.replace(second=59)  # end of search minute
+    ts_dt_start = ts_dt
+    ts_dt_end = ts_dt_start
+    ts_dt_start = ts_dt.replace(second=0)  # start of search minute
+    ts_dt_end = ts_dt_end.replace(second=59)  # end of search minute
 
     # Divide records, search, find and set file entry point to process db records from
     while low_record <= high_record:
-        mid_list = (low_record + high_record) // 2
-        if rows_timestamps[mid_list]['timestamp'] > ts:
-            high_record = mid_list - 1
-        elif rows_timestamps[mid_list]['timestamp'] < ts:
-            low_record = mid_list + 1
-        elif (
-            rows_timestamps[mid_list]['timestamp'] >= ts_start
-            and rows_timestamps[mid_list]['timestamp'] <= ts_end
+        mid_record = (low_record + high_record) // 2
+        if (
+            rows_timestamps[mid_record]['timestamp'] > ts_dt_start
+            and rows_timestamps[mid_record]['timestamp'] > ts_dt_end
         ):
-            return mid_list
+            high_record = mid_record - 1
+        elif (
+            rows_timestamps[mid_record]['timestamp'] < ts_dt_start
+            and rows_timestamps[mid_record]['timestamp'] < ts_dt_end
+        ):
+            low_record = mid_record + 1
+        elif (
+            rows_timestamps[mid_record]['timestamp'] >= ts_dt_start
+            and rows_timestamps[mid_record]['timestamp'] <= ts_dt_end
+        ):
+            # Temporary for testing
+            print("list Index=", mid_record, "File Row=", rows_timestamps[mid_record]['line_number'], rows_timestamps[mid_record]['timestamp'], ts_dt, "Final")
+            return mid_record
         else:
             # This should not happen if the timestamps are sorted correctly
             raise ValueError("Timestamps are not in order, at 1 minute interval, or not in range")
@@ -73,6 +92,7 @@ def record_search(path, delim, field, ts):
 #db_insert
 
 record_search(file_path, field_delimeter, field_index, search_ts)
+
 
 if __name__ == "__main__":
     record_search
