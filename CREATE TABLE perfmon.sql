639e301845fcd91d6732bac26eb5ab98fc5a89b4
Craig <46973909+craigwallace68@users.noreply.github.com>
2025-02-03 19:42:05 -0600

    Update CREATE TABLE perfmon.sql
    Add some extra character space for string fields...

--- CREATE TABLE perfmon.sql
+++ CREATE TABLE perfmon.sql
@@ -1,19 +1,17 @@
 CREATE TABLE perfmon (
-    id SERIAL PRIMARY KEY,
-    host VARCHAR(25) NOT NULL,
-    time_stamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    cpu_inst_util FLOAT4,
-    cpu_avg_util FLOAT4,
-    cpu_max_util FLOAT4,
-    cpu_inst_temp VARCHAR(8),
-    cpu_avg_temp VARCHAR(8),
-    cpu_max_temp VARCHAR(8),
-    disk_usage VARCHAR(5),
-    ram_usage VARCHAR(5),
-    tx_bytes INT,
-    rx_bytes INT,
-    cpu_meas_per_min INT
+	id serial4 NOT NULL,
+	host varchar(25) NOT NULL,
+	"time_stamp" timestamp DEFAULT CURRENT_TIMESTAMP NULL,
+	cpu_inst_util float4 NULL,
+	cpu_avg_util float4 NULL,
+	cpu_max_util float4 NULL,
+	cpu_inst_temp varchar(15) NULL,
+	cpu_avg_temp varchar(15) NULL,
+	cpu_max_temp varchar(15) NULL,
+	disk_usage varchar(10) NULL,
+	ram_usage varchar(10) NULL,
+	tx_bytes int4 NULL,
+	rx_bytes int4 NULL,
+	cpu_meas_per_min int4 NULL,
+	CONSTRAINT perfmon_pkey PRIMARY KEY (id)
 );
-
-insert into perfmon(host, time_stamp, cpu_inst_util, cpu_avg_util, cpu_max_util, cpu_inst_temp, cpu_avg_temp, cpu_max_temp, disk_usage, ram_usage, tx_bytes, rx_bytes, cpu_meas_per_min)
-values('proxmox2', '2025-01-29 11:31:02', 1.24, 1.61, 8.31, '39.0°C', '39.77°C', '42.0°C', '65%', '74%', 10555, 27819, 54);
