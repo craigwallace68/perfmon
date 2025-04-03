b9b1ff19d861eeea0c742cb210202852e5b9d8ed
Craig <46973909+craigwallace68@users.noreply.github.com>
2025-04-03 11:08:28 -0500

    Update db-insert.py
    2 Changes:
      - Add test if no existing sql record exists, and try to establish one
      - Modified tx_bytes and rx_bytes to handle bigint datatype by leveraging the sql autoconvert from string to bigint function upon insert

--- db-insert.py
+++ db-insert.py
@@ -11,6 +11,7 @@ import configparser
 import json
 import re
 
+
 # Suggested crontab entry to run this script and sync with other processes
 # 00:50 * * * * python3 /home/nx2/perfmon_scripts/db-insert.py
 
@@ -83,15 +84,19 @@ def get_last_db_timestamp(db_params, max_ts_str=None):
         cursor.execute(query_max_timestamp)
         result = cursor.fetchone()
         max_db_timestamp = result[0] if result[0] else None
+        
+        # If no current sql record exists, run the rerun function to try and establish one based on current syslog
+        if max_db_timestamp is None:
+            rerun_insert()
         max_ts_str = max_db_timestamp.strftime("%Y-%m-%d %H:%M:%S")
-        return max_ts_str
-
+        
     except Exception as e00:
         print(f"e00: An error occurred: {e00}")
     finally:
         if connection:
             cursor.close()
             connection.close()
+    return max_ts_str
 
 
 
@@ -213,6 +218,11 @@ def insert_parsed_data(db_params, file_path, start_row):
                     for key, value in matches:
                         key_value_pairs[key] = value
 
+                    # Convert tx and rx bytes into strings to bypass standard int range limit
+                    # Let SQL auto convert to bigint upon insert function
+                    tx_bytes = str(key_value_pairs.get('tx_bytes', '0'))
+                    rx_bytes = str(key_value_pairs.get('rx_bytes', '0'))
+
                     # Insert data into the table using a parameterized query
                     insert_query = """
                     INSERT INTO perfmon (
@@ -232,8 +242,8 @@ def insert_parsed_data(db_params, file_path, start_row):
                     key_value_pairs.get('cpu_max_temp', '0'),
                     key_value_pairs.get('disk_usage', '0'),
                     key_value_pairs.get('ram_usage', '0'),
-                    key_value_pairs.get('tx_bytes', '0'),
-                    key_value_pairs.get('rx_bytes', '0'),
+                    tx_bytes,
+                    rx_bytes,
                     key_value_pairs.get('cpu_meas_per_min', '0')
                     ))
                     record_count += 1
